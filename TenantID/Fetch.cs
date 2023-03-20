c5657654d8dfd930148d2b554a52cf3a4e7758a7
MatthijsvanderPlas <mvdplas84@ziggo.nl>
2023-03-20 21:46:51 +0100

    Error Handling

--- TenantID/Fetch.cs
+++ TenantID/Fetch.cs
@@ -1,43 +1,28 @@
-﻿
+﻿using System.Net;
+
 namespace TenantID;
 
 public class Fetch : IFetch
 {
-    public string Url { get; set; }
-    public Guid TenantID { get; set; }
-    public HttpClient HttpClient { get; set; }
-
     public Fetch(string url)
     {
-        this.Url = url;
-        this.HttpClient = new HttpClient();
-        this.TenantID = new Guid();
+        Url = url;
+        HttpClient = new HttpClient();
+        TenantID = "";
     }
-    
-    public Guid? FetchTenantID()
-    {
-        try
-        {
-            HttpResponseMessage response = HttpClient.GetAsync(Url).Result;
-            if (response.IsSuccessStatusCode)
-            {
-                var xml = response.Content.ReadAsStringAsync().Result;
-                var start = xml.IndexOf("entityID", StringComparison.Ordinal);
-                TenantID = new Guid(xml.Substring(start + 34, 36));
-                
-                
-            }
-            else
-            {
-                throw new Exception("Domain not found");
-            }
-   
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-        }
 
+    public string Url { get; set; }
+    public string TenantID { get; set; }
+    public HttpClient HttpClient { get; set; }
+
+    public string FetchTenantID()
+    {
+        var response = HttpClient.GetAsync(Url).Result;
+        if (response.StatusCode == HttpStatusCode.NotFound)
+            return "Domain not found";
+        var xml = response.Content.ReadAsStringAsync().Result;
+        var start = xml.IndexOf("entityID", StringComparison.Ordinal);
+        TenantID = xml.Substring(start + 34, 36);
         return TenantID;
     }
 }
