e882ad6acbfd31da2599904e9713d01bbe459fca
MatthijsvanderPlas <mvdplas84@ziggo.nl>
2023-03-21 09:43:04 +0100

    New start

---
+++ TenantID/Fetch.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using OneOf;
+
+namespace TenantID;
+
+public class Fetch : IFetch
+{
+    public Fetch(string url)
+    {
+        Url = url;
+        HttpClient = new HttpClient();
+    }
+
+    public string Url { get; set; }
+    public Guid TenantID { get; set; }
+    public HttpClient HttpClient { get; set; }
+
+    public OneOf<Guid, ErrorMessage> FetchTenantID()
+    {
+        var response = HttpClient.GetAsync(Url).Result;
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+            return new ErrorMessage("Domain not found!");
+
+        var xml = response.Content.ReadAsStringAsync().Result;
+        var start = xml.IndexOf("entityID", StringComparison.Ordinal);
+        TenantID = new Guid(xml.Substring(start + 34, 36));
+        return TenantID;
+    }
+}
---
+++ TenantID/IFetch.cs
@@ -0,0 +1,12 @@
+﻿using OneOf;
+
+namespace TenantID;
+
+public interface IFetch
+{
+    public HttpClient HttpClient { get; set; }
+    string Url { get; set; }
+    public Guid TenantID { get; set; }
+
+    public OneOf<Guid, ErrorMessage> FetchTenantID();
+}
