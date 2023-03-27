8929654b6e75d9b45c7d2ef6d2682c792098525d
MatthijsvanderPlas <mvdplas84@ziggo.nl>
2023-03-27 13:13:36 +0200

    Works with PowerToys (use --[Tenant Name])

--- TenantID/Program.cs
+++ TenantID/Program.cs
@@ -6,8 +6,18 @@ internal abstract class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Enter domain name:");
-        var domain = Console.ReadLine();
+        var domain = String.Empty;
+        if (args.Length != 0)
+        {
+            domain = args[0].StartsWith("--") ? args[0].Substring(2,args[0].Length-2) : args[0];
+        }
+        else
+        {
+            Console.WriteLine("Enter domain name:");
+            domain = Console.ReadLine();
+            
+        }
+        
         var url = "https://login.microsoftonline.com/" + domain + "/federationmetadata/2007-06/federationmetadata.xml";
 
         var query = new Fetch(url);
@@ -23,7 +33,5 @@ internal abstract class Program
         if (!result.IsT0) return;
         Console.WriteLine($"TenantID: {result.Value} (copied to clipboard)");
         ClipboardService.SetText(result.AsT0.ToString());
-        Console.WriteLine("Hit any key to close...");
-        Console.ReadKey();
-    }
+        }
 }
